46c227cea7d30d8df3d7e832c2c5302ec9bba2d4
Htet Oo Wai Yan <140035139+sc3p73r-it@users.noreply.github.com>
2026-03-29 21:57:44 +0630

    Refactor disk usage check and alert logic

--- bash/mini-projects/1.monitor.sh
+++ bash/mini-projects/1.monitor.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#If the disk usage exceeds 80%, an alert will be displayed.
 
 THRESHOLD=80
 
-usage=$(df / | grep / | awk '{print $5}' | sed 's/%//')
+usage=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-if [ $usage -ge $THRESHOLD ]
-then
-    echo "WARNING: Disk usage is ${usage}%"
+if [ "$usage" -gt "$THRESHOLD" ]; then
+  echo "WARNING: Disk usage is ${usage}%"
 else
-    echo "Disk usage is normal: ${usage}%"
+  echo "Disk usage is normal: ${usage}%"
 fi
